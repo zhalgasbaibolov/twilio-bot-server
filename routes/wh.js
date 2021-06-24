428df72b0e6823cbadce0ee2706dba50d0c4a7a9
karnurmax <karnurmax@mail.ru>
2021-06-24 10:02:36 +0600

    debug

--- routes/wh.js
+++ routes/wh.js
@@ -5,37 +5,40 @@ var router = express.Router();
 router.all('/', function(req, res, next) {
     console.log(req.body)
     const fromNumber = req.body.From;
-    if (fromNumber == 'whatsapp:+14155238886') {
-        return res.send('OK')
+    try {
+        const name = req.body.ProfileName;
+        const msg = req.body.Body;
+        msg = msg.toLowerCase()
+        if (msg == 'main')
+            sendMsg(fromNumber, name, `Main menu\n1) Product menu \n2) Orders menu`)
+        else if (msg == '1')
+            sendMsg(fromNumber, name, `Product menu\n Choose catalog \n 1.1) laptops \n1.2) phones`)
+        else if (msg == '1.1')
+            sendMsg(fromNumber, name, `Catalog: laptops\n1.1.1) HP \n1.1.2) MackPRO`)
+        else if (msg == 'hp' || msg == 'mackpro')
+            sendMsg(fromNumber, name, `Color:\n1) black \n2) white`)
+        else if (msg == 'black' || msg == 'white')
+            sendMsg(fromNumber, name, `Purchase: thank you for your purchase :)`)
+        else if (msg == '2')
+            sendMsg(fromNumber, name, `Order menu\n2.1) Check status \n2) Delete order`)
+        else if (msg == '2.1')
+            sendMsg(fromNumber, name, `Order status: delivering`)
+        else if (msg == '2.2')
+            sendMsg(fromNumber, name, `Order deleting: are you sure you want to delete your order ?[delete/no]`)
+        else if ('delete')
+            sendMsg(fromNumber, name, `Order deleted. Type 'main' to redirect to Main menu`)
+        else if (msg == 'no')
+            sendMsg(fromNumber, name, `Order deleting canceled.Type 'main' to redirect to Main menu`)
+        else
+            sendMsg(fromNumber, name, `please, resend command or type 'main' to redirect to Main menu`)
+        const runMongoCommand = require('../db/mongo')
+        runMongoCommand(insertDocuments, [req.body])
+        res.send("OK");
+    } catch (err) {
+        res.send({
+            err: err
+        })
     }
-    const name = req.body.ProfileName;
-    const msg = req.body.Body;
-    msg = msg.toLowerCase()
-    if (msg == 'main')
-        sendMsg(fromNumber, name, `Main menu\n1) Product menu \n2) Orders menu`)
-    else if (msg == '1')
-        sendMsg(fromNumber, name, `Product menu\n Choose catalog \n 1.1) laptops \n1.2) phones`)
-    else if (msg == '1.1')
-        sendMsg(fromNumber, name, `Catalog: laptops\n1.1.1) HP \n1.1.2) MackPRO`)
-    else if (msg == 'hp' || msg == 'mackpro')
-        sendMsg(fromNumber, name, `Color:\n1) black \n2) white`)
-    else if (msg == 'black' || msg == 'white')
-        sendMsg(fromNumber, name, `Purchase: thank you for your purchase :)`)
-    else if (msg == '2')
-        sendMsg(fromNumber, name, `Order menu\n2.1) Check status \n2) Delete order`)
-    else if (msg == '2.1')
-        sendMsg(fromNumber, name, `Order status: delivering`)
-    else if (msg == '2.2')
-        sendMsg(fromNumber, name, `Order deleting: are you sure you want to delete your order ?[delete/no]`)
-    else if ('delete')
-        sendMsg(fromNumber, name, `Order deleted. Type 'main' to redirect to Main menu`)
-    else if (msg == 'no')
-        sendMsg(fromNumber, name, `Order deleting canceled.Type 'main' to redirect to Main menu`)
-    else
-        sendMsg(fromNumber, name, `please, resend command or type 'main' to redirect to Main menu`)
-    const runMongoCommand = require('../db/mongo')
-    runMongoCommand(insertDocuments, [req.body])
-    res.send("OK");
 });
 
 module.exports = router;
