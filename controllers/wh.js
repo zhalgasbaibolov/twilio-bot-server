2fc4cdc0409a8c645dce34cca1ada9e5eaf0d021
karnurmax <karnurmax@mail.ru>
2021-07-02 18:16:06 +0600

    ...

--- controllers/wh.js
+++ controllers/wh.js
@@ -1,59 +1,61 @@
 const getConnect = require('../db/mongo').getConnect;
 const msgCtrl = require('../controllers/msg')
-const ctrl = {
-    msg(req, res) {
-        try {
-            const {
-                From: fromNumber,
-                Body: msg
-            } = req.body;
-            if (!fromNumber || !msg) {
-                msgCtrl.sendMsg({
-                    fromNumber,
-                    msg: JSON.stringify(req.body)
-                })
-                return res.status(406).send({
-                    msg: 'wrong msg'
-                })
-            }
-            const client = getConnect();
-            client.connect(connecionError => {
-                if (connecionError) {
-                    console.log(connecionError)
-                    return res.status(200).send(connecionError)
-                }
-                const db = client.db("test");
-                const userStates = db.collection('userStates');
-                userStates.findOne({
-                        phone: fromNumber
-                    }).then(state => {
-                        msgCtrl.sendMsg({
-                            fromNumber,
-                            msg: JSON.stringify(state)
-                        })
-                        return res.send(state || 'not found last active')
 
-                    })
-                    .catch(err => {
-                        msgCtrl.sendMsg({
-                            fromNumber,
-                            msg: JSON.stringify(err)
-                        })
-                        return res.status(200).send(err)
-                    })
-                    .finally(() => {
-                        client.close();
-                    })
-            });
-        } catch (err) {
+const msg = function(req, res) {
+    console.log('wh controller', req)
+    try {
+        const {
+            From: fromNumber,
+            Body: msg
+        } = req.body;
+        if (!fromNumber || !msg) {
             msgCtrl.sendMsg({
                 fromNumber,
-                msg: JSON.stringify(err)
+                msg: JSON.stringify(req.body)
             })
-            return res.status(200).send(err)
+            return res.status(406).send({
+                msg: 'wrong msg'
+            })
         }
+        const client = getConnect();
+        client.connect(connecionError => {
+            if (connecionError) {
+                console.log(connecionError)
+                return res.status(200).send(connecionError)
+            }
+            const db = client.db("test");
+            const userStates = db.collection('userStates');
+            userStates.findOne({
+                    phone: fromNumber
+                }).then(state => {
+                    msgCtrl.sendMsg({
+                        fromNumber,
+                        msg: JSON.stringify(state)
+                    })
+                    return res.send(state || 'not found last active')
 
-    },
+                })
+                .catch(err => {
+                    msgCtrl.sendMsg({
+                        fromNumber,
+                        msg: JSON.stringify(err)
+                    })
+                    return res.status(200).send(err)
+                })
+                .finally(() => {
+                    client.close();
+                })
+        });
+    } catch (err) {
+        msgCtrl.sendMsg({
+            fromNumber,
+            msg: JSON.stringify(err)
+        })
+        return res.status(200).send(err)
+    }
+
+}
+
+module.exports = {
+    msg
 };
-
-module.exports = ctrl;
