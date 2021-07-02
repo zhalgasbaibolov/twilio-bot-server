d52ac3d5aa5ce9f8123b5080472aacf3fd3759a2
karnurmax <karnurmax@mail.ru>
2021-07-02 15:15:44 +0600

    log error

--- controllers/wh.js
+++ controllers/wh.js
@@ -2,21 +2,24 @@ const getConnect = require('../db/mongo').getConnect;
 const msgCtrl = require('../controllers/msg')
 const ctrl = {
     msg(req, res) {
-
-        const {
-            From: fromNumber,
-            Body: msg
-        } = req.body;
-        if (!fromNumber || !msg) {
-            return res.status(400).send({
-                msg: 'wrong msg'
+        try {
+            const {
+                From: fromNumber,
+                Body: msg
+            } = req.body;
+            if (!fromNumber || !msg) {
+                return res.status(400).send({
+                    msg: 'wrong msg'
+                })
+            }
+            msgCtrl.sendMsg({
+                fromNumber,
+                msg
             })
+        } catch (err) {
+            console.log(err)
         }
-        msgCtrl.sendMsg({
-            fromNumber,
-            msg
-        })
-        return;
+        return res.sendStatus(200)
         const client = getConnect();
         client.connect(connecionError => {
             if (connecionError) {
