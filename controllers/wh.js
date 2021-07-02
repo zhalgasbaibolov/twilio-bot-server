037f9d3e08657436b5ad0fb042ea8446ba20e374
karnurmax <karnurmax@mail.ru>
2021-07-02 18:49:12 +0600

    ...

--- controllers/wh.js
+++ controllers/wh.js
@@ -5,26 +5,31 @@ const msg = function(req, res) {
     try {
         const fromNumber = req.body.From || req.body['From'];
         if ('whatsapp:+14155238886' === fromNumber)
-            return res.send(200)
+            return res.sendStatus(200)
         const msg = req.body.Body || req.body['Body'];
         console.log('wh controller', fromNumber, msg, req.body)
 
-        if (!fromNumber || !msg) {
-            msgCtrl.sendMsg({
-                fromNumber,
-                msg: JSON.stringify({
-                    fromNumber,
-                    msg
-                })
-            })
-            return res.status(406).send({
-                msg: 'wrong msg'
-            })
-        }
+        // if (!fromNumber || !msg) {
+        //     msgCtrl.sendMsg({
+        //         fromNumber,
+        //         msg: JSON.stringify({
+        //             fromNumber,
+        //             msg
+        //         })
+        //     })
+        //     console.log('return 406 from wh ctrl')
+        //     return res.status(406).send({
+        //         msg: 'wrong msg'
+        //     })
+        // }
         const client = getConnect();
         client.connect(connecionError => {
             if (connecionError) {
                 console.log(connecionError)
+                msgCtrl.sendMsg({
+                    fromNumber,
+                    msg: JSON.stringify(connecionError)
+                })
                 return res.status(200).send(connecionError)
             }
             const db = client.db("test");
