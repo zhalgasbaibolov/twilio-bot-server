859cbb2aaf3b63454775d8e43dcd0bae4d67e0b0
karnurmax <karnurmax@mail.ru>
2021-07-09 13:39:13 +0600

    get lineItems on create new checkout

--- controllers/storefrontAPI.js
+++ controllers/storefrontAPI.js
@@ -135,6 +135,7 @@ const createCheckout = async(storeMyShopify, accessToken, variantId) => {
         checkout {
           id
           webUrl
+          lineItems
         }
         checkoutUserErrors {
           code
