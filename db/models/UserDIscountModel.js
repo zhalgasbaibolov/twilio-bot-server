790b03bb2936734d2e0eceb5f1bb5754f5ab9feb
karnurmax <karnurmax@mail.ru>
2021-07-16 13:40:04 +0600

    remove extra mongo connection in DiscountModel

--- db/models/UserDIscountModel.js
+++ db/models/UserDIscountModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-// Set up default mongoose connection
-const mongoDB = 'mongodb+srv://nurlan:qweQWE123@cluster0.ikiuf.mongodb.net/twiliodb?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Get the default connection
 const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
