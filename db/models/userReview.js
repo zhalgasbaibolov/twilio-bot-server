98fca9acb04796dbc334f738c1e35bf71e164bf1
Zhalgas Baibolov <zhalgasbaibolov@.com>
2021-07-19 14:53:54 +0600

    edited userreview model

--- db/models/userReview.js
+++ db/models/userReview.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
-// Set up default mongoose connection
-const mongoDB = 'mongodb+srv://nurlan:qweQWE123@cluster0.ikiuf.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Get the default connection
 const db = mongoose.connection;
 
-// Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const { Schema } = mongoose;
