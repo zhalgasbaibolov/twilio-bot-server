c4676e8d37d35f688a0b7dad8d59c3435abbe2e0
karnurmax <karnurmax@mail.ru>
2021-07-19 18:21:19 +0600

    app.js run lint

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const settingsRouter = require('./routes/settings');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -30,18 +29,14 @@ app.use('/wh', whRouter);
 app.use('/blocks', blockRouter);
 app.use('/settings', settingsRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
