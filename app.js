e084144f4245f3a4e316f4e6600c847c2a9a6206
karnurmax <karnurmax@mail.ru>
2021-07-22 14:46:13 +0600

    tracker on db open

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const db = mongoose.connection;
 
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', tracker);
 
-tracker();
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
