939d3b1c66367e689927bbb03f8ca4addb39eafe
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-07-30 10:57:38 +0600

    edited viewcart logic

--- controllers/wh.js
+++ controllers/wh.js
@@ -184,28 +184,16 @@ async function handleMessage(req, res) {
   };
 
   const sendViewCart = () => {
-      const storedLineItemsText = state.storedLineItems
-        .filter((x) => x.title && x.quantity)
-        .map(
-          ({ title, quantity, productTitle }, idx) => `${idx + 1}. ${productTitle}, ${title}, quantity: *${quantity}*`,
-        )
-        .join('\n');
-
-      const txt = `Your cart is:\n${storedLineItemsText}\n\n\nWhat do you want to do next?\n1. Continue Shopping \n2. Proceed to payment \n3. Delete item\n--------------\n0. Back to main menu`;
-      msgCtrl.sendMsg({
-        fromNumber,
-        msg: txt,
-      });
-      UserState.updateOne(
-        {
-          phone: fromNumber,
+    UserState.updateOne(
+      {
+        phone: fromNumber,
+      },
+      {
+        $set: {
+          last: 'cartView',
         },
-        {
-          $set: {
-            last: 'cart',
-          },
-        },
-      ).exec();
+      },
+    ).exec();
   };
 
   function sendDiscount() {
@@ -321,6 +309,29 @@ async function handleMessage(req, res) {
           text: msg,
         })
         .then(sendMainMenu).catch(errorHandler);
+    } else if (state.last === 'cartView') {
+      const storedLineItemsText = state.storedLineItems
+        .filter((x) => x.title && x.quantity)
+        .map(
+          ({ title, quantity, productTitle }, idx) => `${idx + 1}. ${productTitle}, ${title}, quantity: *${quantity}*`,
+        )
+        .join('\n');
+
+      const txt = `Your cart is:\n${storedLineItemsText}\n\n\nWhat do you want to do next?\n1. Continue Shopping \n2. Proceed to payment \n3. Delete item\n--------------\n0. Back to main menu`;
+      msgCtrl.sendMsg({
+        fromNumber,
+        msg: txt,
+      });
+      UserState.updateOne(
+        {
+          phone: fromNumber,
+        },
+        {
+          $set: {
+            last: 'cart',
+          },
+        },
+      ).exec();
     } else if (state.last === 'marketing') {
       switch (msg) {
         case '1': {
@@ -649,11 +660,11 @@ async function handleMessage(req, res) {
     } else if (state.last === 'deleteItem') {
       state.storedLineItems.splice(msg - 1, 1);
       const storedLineItemsText = state.storedLineItems
-              .filter((x) => x.title && x.quantity)
-              .map(
-                ({ title, quantity, productTitle }, idx) => `${idx + 1}. ${productTitle}, ${title}, quantity: *${quantity}*`,
-              )
-              .join('\n');
+        .filter((x) => x.title && x.quantity)
+        .map(
+          ({ title, quantity, productTitle }, idx) => `${idx + 1}. ${productTitle}, ${title}, quantity: *${quantity}*`,
+        )
+        .join('\n');
       const txt = `Your cart is:\n${storedLineItemsText}\n\n\nWhat do you want to do next?\n1. Continue Shopping \n2. Proceed to payment \n3. Delete item\n--------------\n0. Back to main menu`;
       msgCtrl.sendMsg({
         fromNumber,
