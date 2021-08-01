84ce97350bdd61e3eea9d28cdb72836302980e12
karnurmax <karnurmax@mail.ru>
2021-08-01 23:46:51 +0600

    ...

--- db/models/UserGetSupport.js
+++ db/models/UserGetSupport.js
@@ -6,7 +6,8 @@ const { Schema } = mongoose;
 const userGetSupport = new Schema({
   profileName: { type: String, requried: false },
   accountSid: { type: String, required: true },
-  phone: { type: String, required: true },
+  whatsappNumber: { type: String, required: true },
+  owner: { type: String, required: true, default: 'user' },
   text: { type: String },
 }, {
   collection: 'userGetSupports',
