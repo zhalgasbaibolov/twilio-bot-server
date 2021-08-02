af57080167dddd5793f9ab32057a25e19119f67f
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-02 12:40:48 +0600

    tracker discounts file created from tracker

---
+++ trackerDiscount.js
@@ -0,0 +1,117 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+const UserDiscount = require('./db/models/UserDiscount');
+const UserSetting = require('./db/models/UserSettings');
+const { WhatsapSender } = require('./providers/WhatsapSender');
+// const { handleMessage } = require('./controllers/wh');
+
+const a = '370a717f';
+const token = `${a}84299f15e25757c7e3e627fa`;
+const msgCtrl = WhatsapSender({
+  accountSid:
+  'AC534b07c807465b936b2241514b536512',
+  authToken:
+  token,
+});
+const {
+  getActivatedDiscounts,
+} = require('./getActivatedDiscounts');
+
+module.exports.tracker = () => {
+  setInterval(() => {
+    UserSetting.find({}).exec()
+      .then((arr) => {
+        if (!arr || !arr.length) return;
+        arr.forEach((sett) => {
+          const {
+            storeMyShopify,
+            apiVersion,
+            storeAPIkey,
+            storePassword,
+          } = sett.shopify;
+          getAbandonedCart(
+            storeMyShopify,
+            apiVersion,
+            storeAPIkey,
+            storePassword,
+          )
+            .then((response) => {
+              let allCarts = response.data && response.data.checkouts;
+              if (!allCarts || !allCarts.length) {
+                console.log('abandoned carts not found');
+                return;
+              }
+              allCarts = allCarts.filter((cart) => cart.discount_codes
+         && cart.discount_codes.length);
+              // console.log(allCarts);
+              UserDiscount.find({
+                notifiedCount: {
+                  $lt: 1,
+                },
+              }, (err, pairs) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                if (!pairs || !pairs.length) {
+                  console.log('phone:discount pairs not found');
+                  return;
+                }
+                allCarts.forEach((cart) => {
+                  for (let i = 0; i < cart.discount_codes.length; i += 1) {
+                    const { code } = cart.discount_codes[i];
+                    const findedPair = pairs.find((p) => p.discountCode === code);
+                    if (!findedPair) {
+                      return;
+                    }
+                    console.log('******************************');
+                    console.log(`found pairs: ${findedPair.phone}: ${findedPair.discountCode}: ${findedPair.notifiedCount}`,
+                      cart.abandoned_checkout_url);
+                    console.log('******************************');
+
+                    msgCtrl.sendMsg({
+                      fromNumber: findedPair.phone,
+                      msg: `Hi! We noticed that you left a few items in your shopping cart.\nPlease check it:\n${
+                        cart.abandoned_checkout_url}`,
+                    });
+                    setTimeout(() => {
+                      const txt = cart.line_items.map(({ title, variant_title, quantity }, idx) => `${idx + 1}. ${title}, ${variant_title}, quantity: ${quantity}.`).join('\n');
+                      msgCtrl.sendMsg({
+                        fromNumber: findedPair.phone,
+                        msg: `Your cart is:\n${txt}`,
+                      });
+                      setTimeout(() => {
+                        msgCtrl.sendMsg({
+                          fromNumber: findedPair.phone,
+                          // msg: `${handleMessage.sendMainMenu(0, true)}`,
+                          msg: 'What would you like to do now?\n1. Catalog\n2. Customer Support\n3. Order Status\n4. Abandoned cart\n5. Loyalty program (organic marketing)\n\n\n(Please, type the number corresponding to your choice)',
+
+                        });
+                      }, 8000);
+                    }, 4000);
+
+                    UserDiscount.updateOne({
+                      discountCode: findedPair.discountCode,
+                      phone: findedPair.phone,
+                    }, {
+                      notifiedCount: 2,
+                    }, {}, (err2, upd) => {
+                      if (err2) {
+                        console.log(err2);
+                      }
+                      if (upd.ok) console.log(upd.ok === 1);
+                    });
+                    return;
+                  }
+                });
+                // eslint-disable-next-line consistent-return
+                return pairs;
+              });
+            }).catch((err) => {
+              console.log(err);
+            });
+        });
+      })
+      .catch((err) => { console.log(err); });
+  }, 5000);
+};
