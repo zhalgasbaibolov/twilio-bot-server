339d3870538adf7b3c26914cf25f2b001c85490d
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-02 10:40:09 +0600

    discount code changed from random string to number

--- db/models/UserDiscount.js
+++ db/models/UserDiscount.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 const userDiscountSchema = new Schema({
   phone: { type: String, required: true },
   discountCode: { type: String, required: true },
+  accountSiD: {type: String, required: true},
   notifiedCount: { type: Number, default: 0 },
 }, {
   collection: 'userDiscount',
