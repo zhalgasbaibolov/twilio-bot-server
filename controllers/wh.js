a4570a4ab1109b96bc502af793cc0bdc0b69d101
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-04 15:16:34 +0600

    some lexical improvements

--- controllers/wh.js
+++ controllers/wh.js
@@ -133,7 +133,7 @@ async function handleMessage(req, res) {
   const sendMarketing = () => {
     msgCtrl.sendMsg({
       fromNumber,
-      msg: 'Would you like to leave us a review for 5 points?\n1. Yes\n2. No',
+      msg: 'We\'d love to hear your review! Got a minute to share it with us?\n1. Yes\n2. No',
     });
     UserState.updateOne(
       {
@@ -337,7 +337,7 @@ async function handleMessage(req, res) {
         case '1': {
           msgCtrl.sendMsg({
             fromNumber,
-            msg: 'Please type your review:',
+            msg: 'Please type your feedback:',
           });
           UserState.updateOne(
             {
