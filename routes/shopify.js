17d38f4872a855390dcbabd559f22540f1b3119d
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-04 16:33:08 +0600

    res send 200 OK

--- routes/shopify.js
+++ routes/shopify.js
@@ -6,11 +6,11 @@ const router = express.Router();
 const shopifyTest = require('../shopifyTest');
 
 router.post('/webhooks/fulfillments/create', async (req, res) => {
+  res.status(200);
   shopifyTest();
   // console.log('🎉 We got a fulfillment create!');
   // const hmac = req.get('X-Shopify-Hmac-Sha256');
   // const body = await getRawBody(req);
-  return res.status(200);
 });
 
 module.exports = router;
