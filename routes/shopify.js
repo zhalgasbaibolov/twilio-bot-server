99607a4d810339e28a9a97424dd72642a046c8fe
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-04 15:22:16 +0600

    webhook/fulfillment

--- routes/shopify.js
+++ routes/shopify.js
@@ -5,10 +5,11 @@ const express = require('express');
 const router = express.Router();
 const shopifyCtrl = require('../controllers/shopifywebhook');
 
-router.all('/', async (req, res) => {
-  await shopifyCtrl.handleMessage(req, res);
+router.post('/webhooks/fulfillments/create', async (req, res) => {
+  console.log('🎉 We got a fulfillment create!');
+  // const hmac = req.get('X-Shopify-Hmac-Sha256');
+  // const body = await getRawBody(req);
+  return res.status(200);
 });
-
-router.all('/status', shopifyCtrl.handleStatus);
 
 module.exports = router;
