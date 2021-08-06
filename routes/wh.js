f04a6fcbc384431fed8a72ee3fdd90d2fa910d05
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-06 10:59:36 +0600

    editings

--- routes/wh.js
+++ routes/wh.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const whCtrl = require('../controllers/wh/index');
+const whCtrl = require('../controllers/wh');
 
 router.all('/', async (req, res) => {
   await whCtrl.handleMessage(req, res);
