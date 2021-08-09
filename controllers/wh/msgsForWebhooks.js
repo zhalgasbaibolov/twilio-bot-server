ba0b4d8aa5cd42e4d246ff6fff04b9daa01e1637
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-09 09:47:14 +0600

    business logic improvements

--- controllers/wh/msgsForWebhooks.js
+++ controllers/wh/msgsForWebhooks.js
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
 const { WhatsapSender } = require('../../providers/WhatsapSender');
-const whCtrl = require('./index');
 
 const a = '370a717f';
 const token = `${a}84299f15e25757c7e3e627fa`;
@@ -17,12 +16,18 @@ const shopifyOrderCreated = (phoneNumber, userName, orderNumber) => {
     fromNumber: `whatsapp:${phoneNumber}`,
     msg: `Hello, ${userName}!\nThank you for your shopping with us!\nYour order #${orderNumber} has been received.\n\nWe'll send tracking information when order ships.`,
   });
+  setTimeout(() => {
+    msgCtrl.sendMsg({
+      fromNumber: `whatsapp:${phoneNumber}`,
+      msg: 'We\'d love to hear your review! Got a minute to share it with us?\n1. Yes\n2. No',
+    });
+  }, 3000);
 };
 
 const shopifyFulfillmentCreated = (phoneNumber, userName, trackingNumber) => {
   msgCtrl.sendMsg({
     fromNumber: `whatsapp:${phoneNumber}`,
-    msg: `Hello, ${userName}!\nYour order has shipped.\nThis is your tracking number: ${trackingNumber}\n\nUse this link to track your package: https://t.17track.net/en#nums=${trackingNumber}`,
+    msg: `Hello, ${userName}!\n\nWe're happy to tell you that your order has shipped!\n\nThis is your tracking number: ${trackingNumber}\n\nUse this link to track your package: https://t.17track.net/en#nums=${trackingNumber}`,
   });
 };
 
