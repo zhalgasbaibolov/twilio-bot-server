c3e061f1ccbd85709c2879c30c77cd51c541322f
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-10 11:26:14 +0600

    changed fromnumber in webhook msgs

--- controllers/wh/msgsForWebhooks.js
+++ controllers/wh/msgsForWebhooks.js
@@ -16,16 +16,17 @@ const backToMenu = '--------------\n0. Back to main menu';
 const typeRecomendation = '(Please, type the number corresponding to your choice)';
 
 const shopifyOrderCreated = (phoneNumber, userName, orderNumber) => {
-  const fromNumber = phoneNumber;
+  const fromNumber = `whatsapp:${phoneNumber}`;
   msgCtrl.sendMsg({
-    fromNumber: `whatsapp:${fromNumber}`,
+    fromNumber: fromNumber,
     msg: `Hello, ${userName}!\nThank you for your shopping with us!\nYour order #${orderNumber} has been received.\n\nWe'll send tracking information when order ships.`,
   });
   setTimeout(() => {
     msgCtrl.sendMsg({
-      fromNumber: `whatsapp:${fromNumber}`,
+      fromNumber: fromNumber,
       msg: `We'd love to hear your review! Got a minute to share it with us?\n1. Yes\n2. No\n\n${backToMenu}\n${typeRecomendation}`,
     });
+
     UserState.updateOne(
       {
         phone: fromNumber,
@@ -40,14 +41,14 @@ const shopifyOrderCreated = (phoneNumber, userName, orderNumber) => {
 };
 
 const shopifyFulfillmentCreated = (phoneNumber, userName, trackingNumber) => {
-  const fromNumber = phoneNumber;
+  const fromNumber = `whatsapp:${phoneNumber}`;
   msgCtrl.sendMsg({
-    fromNumber: `whatsapp:${phoneNumber}`,
+    fromNumber: fromNumber,
     msg: `Hello, ${userName}!\n\nWe're happy to tell you that your order has shipped!\n\nThis is your tracking number: ${trackingNumber}\n\nUse this link to track your package: https://t.17track.net/en#nums=${trackingNumber}\n\n${backToMenu}\n${typeRecomendation}`,
   });
   setTimeout(() => {
     msgCtrl.sendMsg({
-      fromNumber: `whatsapp:${fromNumber}`,
+      fromNumber: fromNumber,
       msg: `We'd love to hear your review! Got a minute to share it with us?\n1. Yes\n2. No\n\n${backToMenu}\n${typeRecomendation}`,
     });
     UserState.updateOne(
