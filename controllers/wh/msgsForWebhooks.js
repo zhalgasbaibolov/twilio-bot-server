58dd009c21dd435df8db031bdf5782a87de61e2e
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-11 14:15:43 +0600

    edited console log for discount

--- controllers/wh/msgsForWebhooks.js
+++ controllers/wh/msgsForWebhooks.js
@@ -92,7 +92,6 @@ async function onShopifyDiscountActivated(discountCodeFromHook) {
       }
       console.log(`\n\n\n\n\*****************\ndiscount code from hook: ${discountCodeFromHook}\n*********************\n\n\n\n`);
       const foundPair = pairs.find((p) => p.discountCode === discountCodeFromHook);
-      console.log(`\n\n\n\n\*****************\ndiscount code from server: ${discountCode}\n*********************\n\n\n\n`);
 
       const discountSlug = generateSlug();
       if (!foundPair) {
