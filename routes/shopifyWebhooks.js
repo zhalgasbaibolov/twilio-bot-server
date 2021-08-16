383610d2261fcfb1341736ea74aa07f16f87daff
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-16 16:40:51 +0600

    added phone number

--- routes/shopifyWebhooks.js
+++ routes/shopifyWebhooks.js
@@ -10,10 +10,8 @@ const {
 
 router.post('/webhooks/fulfillments/create', async (req, res) => {
   res.send('OK');
-  const phoneNumber = '+77075002029';
-  // // req.body.destination.flat().map((ord) => ord.phone);
-  const userName = 'zhalgas';
-  // // req.body.destination.first_name;
+  const phoneNumber = req.body.destination.phone;
+  const userName = req.body.destination.first_name;
   const trackingNumber = req.body.tracking_number;
   const trackingUrl = req.body.tracking_url;
 
