92b866edfc50de715e2fb73ad7595e1fb5374937
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-19 14:45:11 +0600

    flat method media url list

--- controllers/wh/index.js
+++ controllers/wh/index.js
@@ -517,9 +517,9 @@ async function handleMessage(req, res) {
             const v = variants[i];
             v.productTitle = productTitle;
           }
-          const mediaUrlList = variants.filter((el) => el != null).map(
+          const mediaUrlList = variants.map(
             (item) => item.node.image && item.node.image.originalSrc,
-          );
+          ).flat();
           console.log(`\n\n\n*******************\nMedia list: ${mediaUrlList}\n*******************\n\n\n`);
           if (mediaUrlList && mediaUrlList.length) {
             msgCtrl.sendMediaList({
