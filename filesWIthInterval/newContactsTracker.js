d4e1fae7fbf4afdd9467109850ff8b54e7e0f76a
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-19 16:39:37 +0600

    new file for searching new contacts

---
+++ filesWIthInterval/newContactsTracker.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+// const UserSetting = require('../db/models/UserSetting');
+// const UserState = require('../db/models/UserState');
+// const axios = require('axios');
+// const UserContact = require('../db/models/UserContact');
+
+const intervalTime = 3000; // 1 hour
+
+function newContactsTracker() {
+  setInterval(() => {
+
+  }, intervalTime);
+}
+
+module.exports = {
+  newContactsTracker,
+};
