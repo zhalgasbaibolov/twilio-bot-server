9b8251a541cc9d1480ad8ef4c9995b732b8400ee
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-19 12:36:54 +0600

    editing permalink

--- controllers/wh/index.js
+++ controllers/wh/index.js
@@ -94,7 +94,7 @@ async function handleMessage(req, res) {
     });
   }
   function createCheckoutWithDiscount(state) {
-    const checkoutUrl = state.storedLineItems.map(({ variantId, quantity }) => `${variantId.slice(28)}:${quantity}`).join('');
+    const checkoutUrl = state.storedLineItems.map(({ variantId, quantity }) => `${variantId.slice(29)}:${quantity}`).join(',').slice(-1);
 
     const discountSlug = generateSlug();
     shopifyApi.shopifyDiscountCreate(
