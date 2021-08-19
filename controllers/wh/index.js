00ed6008eb68a0d724d46832a64e176ab4f2ac45
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-19 12:01:17 +0600

    debugging permalink

--- controllers/wh/index.js
+++ controllers/wh/index.js
@@ -94,7 +94,9 @@ async function handleMessage(req, res) {
     });
   }
   function createCheckoutWithDiscount(state) {
-    const checkoutUrl = state.storedLineItems.map(({ variantID, quantity }) => `${variantID.slice(28)}:${quantity}`).join('');
+    const checkoutUrl = state.storedLineItems.map(({ variantID, quantity }) => (
+      console.log(`\n\n\n++++++++++\nvariantId is: ${variantID}\n++++++++++\n\n\n`)
+      `${variantID.slice(28)}:${quantity}`)).join('');
 
     const discountSlug = generateSlug();
     shopifyApi.shopifyDiscountCreate(
