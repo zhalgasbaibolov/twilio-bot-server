09b162a7cceaec45787cf029f6967fa20c530845
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-20 09:57:05 +0600

    diabled abandoned checkout

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('open', () => {
-  tracker();
+  // tracker();
 
   // trackerDiscount();
   // trackerSelf();
