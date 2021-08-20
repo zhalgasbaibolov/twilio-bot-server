361280ff77babac79c5d58e6f9ca559a41a4b793
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-20 16:45:45 +0600

    disabled new contacts tracker

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const { tracker } = require('./filesWIthInterval/abandonedCartsTracker');
-const { newContactsTracker } = require('./filesWIthInterval/newContactsTracker');
-// const { trackerDiscount } = require('./trackerDiscount');
-// const { trackerSelf } = require('./trackerSelf');
+// const { newContactsTracker } = require('./filesWIthInterval/newContactsTracker');
 
 // Set up default mongoose connection
 const mongoDB = 'mongodb+srv://nurlan:qweQWE123@cluster0.ikiuf.mongodb.net/test?retryWrites=true&w=majority';
@@ -21,10 +19,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('open', () => {
   tracker();
-  newContactsTracker();
-
-  // trackerDiscount();
-  // trackerSelf();
+  // newContactsTracker();
 });
 
 const indexRouter = require('./routes/index');
