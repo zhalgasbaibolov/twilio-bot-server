2fc34cf77105f82e94cd34de3e8460a1e5470e85
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-20 11:19:52 +0600

    new function for searching new contacts from json

--- filesWIthInterval/newContactsTracker.js
+++ filesWIthInterval/newContactsTracker.js
@@ -1,15 +1,47 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
-// const UserSetting = require('../db/models/UserSetting');
-// const UserState = require('../db/models/UserState');
-// const axios = require('axios');
-// const UserContact = require('../db/models/UserContact');
+const { getProviders } = require('../providers');
+const UserContact = require('../db/models/UserContact');
 
 const intervalTime = 3000; // 1 hour
 
-function newContactsTracker() {
+async function newContactsTracker(req, res) {
+  res.send('OK');
+  const getProviderResult = await getProviders(req);
+  if (!getProviderResult) {
+    return;
+  }
+  const { shopifyApi } = getProviderResult;
   setInterval(() => {
+    shopifyApi.getAllOrders()
+      .then((response) => {
+        const phoneNumbers = response.data.orders
+          .map((ord) => ord.phone);
+        if (!phoneNumbers) {
+          return;
+        }
+        for (let i = 0; i < phoneNumbers.length; i += 1) {
+          UserContact.find({ phone: phoneNumbers[i] }, (err, pairs) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            if (!pairs || !pairs.length) {
+              console.log('there is no phone number in orders.json');
+            }
 
+            const foundPair = pairs.find((p) => p.phone !== phoneNumbers[i]);
+            UserContact
+              .create({
+                phone: foundPair.phone,
+                contactType: 'fromShopifyDB',
+              });
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, intervalTime);
 }
 
