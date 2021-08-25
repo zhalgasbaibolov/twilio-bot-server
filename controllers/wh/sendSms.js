574aca27ade828f3b3b39fb367e8e3b140a6e7fe
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-25 13:49:32 +0600

    edited route

--- controllers/wh/sendSms.js
+++ controllers/wh/sendSms.js
@@ -8,11 +8,18 @@ const accountSid = 'AC4352390b9be632aabb39a3b9282dc338';
 const client = require('twilio')(accountSid, authToken);
 
 function sendSms(phoneNumber, message) {
-  console.log(`\n\n+-+-+-+-+-+-+-+\naccepted values: 1. phoneNumber - ${phoneNumber}, 2. message - ${message}\n+-+-+-+-+-+-+-\n\n`);
-  client.messages.create({
-    body: message,
-    from: twilioNumber,
-    to: phoneNumber,
+  return new Promise((resolve, reject) => {
+    try {
+      console.log(`\n\n+-+-+-+-+-+-+-+\naccepted values: 1. phoneNumber - ${phoneNumber}, 2. message - ${message}\n+-+-+-+-+-+-+-\n\n`);
+      const response = client.messages.create({
+        body: message,
+        from: twilioNumber,
+        to: phoneNumber,
+      });
+      resolve(response);
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
