60a7d9510330ea0a2aa1949f475cfa1644703ab7
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-25 10:42:44 +0600

    changed logic in sms sending

---
+++ routes/smsRouter.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const router = express.Router();
+const {
+  sendSms,
+} = require('../controllers/wh/sendSms');
+
+router.post('/sms', async (req, res) => {
+  res.send('OK');
+  const phoneNumber = req.body.to;
+  const { message } = req.body;
+
+  if (!phoneNumber || !message) {
+    console.log('\n\n\n\n++++++++++++++++\nthere is no phone number OR message!\n++++++++++++++++\n\n\n\n');
+  }
+
+  sendSms(phoneNumber, message);
+});
+
+module.exports = router;
