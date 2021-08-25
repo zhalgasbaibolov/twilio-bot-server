277677b99e34c89a56c3e6f3fc2d2b01f0e2cb19
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-25 13:19:32 +0600

    await send sms

--- routes/smsRouter.js
+++ routes/smsRouter.js
@@ -15,10 +15,9 @@ router.post('/sms', async (req, res) => {
   if (!phoneNumber || !message) {
     console.log('\n\n\n\n++++++++++++++++\nthere is no phone number OR message!\n++++++++++++++++\n\n\n\n');
   }
-  console.log(`\n\n-*-*-*-*-*-*\nsmsRouter.js\nsending sms to: ${phoneNumber} with message: ${message}\n-*-*-*-*-*-*-\n\n`).then(() => { sendSms(phoneNumber, message); }).catch((error) => {
-    console.log(error);
-    return false;
-  });
+  console.log(`\n\n-*-*-*-*-*-*\nsmsRouter.js\nsending sms to: ${phoneNumber} with message: ${message}\n-*-*-*-*-*-*-\n\n`)
+
+   await sendSms(phoneNumber, message);
 });
 
 module.exports = router;
