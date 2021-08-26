34e4084efeac27844fa31e0a03eee1a39a910a3f
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 16:49:29 +0600

    deleted async from user contacts

--- getAllContacts.js
+++ getAllContacts.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 const UserContacts = require('./db/models/UserContact');
 
-async function getAllContacts() {
-  const contacts = await UserContacts.find({}).exec();
+function getAllContacts() {
+  const contacts = UserContacts.find({}).exec();
 
   return contacts;
 }
