5b23138184d9868c04278db434e6301c93c15617
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 15:28:50 +0600

    token -> authtoken

--- controllers/wh/sendSms.js
+++ controllers/wh/sendSms.js
@@ -3,7 +3,7 @@ const twilioNumber = '+19403146668';
 const a = 'be22960c1';
 const b = 'a28fe7d3aa41';
 const c = '4fe4998e108';
-const token = `${a}${b}${c}`;
+const authToken = `${a}${b}${c}`;
 const accountSid = 'AC09da4ce2aced21a4636bb3e288633b0d';
 const client = require('twilio')(accountSid, authToken);
 
