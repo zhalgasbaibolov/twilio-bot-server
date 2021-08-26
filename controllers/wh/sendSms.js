f0524f8ccf6bef6bb165b9dbd0ebdfe3fd63b8e5
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 15:27:41 +0600

    changed sms sender

--- controllers/wh/sendSms.js
+++ controllers/wh/sendSms.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
-const twilioNumber = '+19286156092';
-const a = 'abdc276b';
-const b = 'ca5995f447';
-const c = 'd05df1e9610526';
-const authToken = `${a}${b}${c}`;
-const accountSid = 'AC4352390b9be632aabb39a3b9282dc338';
+const twilioNumber = '+19403146668';
+const a = 'be22960c1';
+const b = 'a28fe7d3aa41';
+const c = '4fe4998e108';
+const token = `${a}${b}${c}`;
+const accountSid = 'AC09da4ce2aced21a4636bb3e288633b0d';
 const client = require('twilio')(accountSid, authToken);
 
 function sendSms(phoneNumber, message) {
