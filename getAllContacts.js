719e6b2e1b9c905c12b209e6ac7cb85c27e4a3a0
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 17:36:25 +0600

    ...

--- getAllContacts.js
+++ getAllContacts.js
@@ -4,8 +4,8 @@ const UserContacts = require('./db/models/UserContact');
 function getAllContacts() {
   const arr = [];
   const contacts = UserContacts.find({}).exec();
-  console.log(typeof(contacts))
-  console.log(contacts)
+  console.log(typeof (contacts));
+  console.log(contacts);
   arr.push(contacts.map((x) => x.phone));
 
   return arr;
