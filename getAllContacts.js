8ba452297f03880fa3a7fafefba18bfce2b44245
zhalgasbaibolov <zhalgasbaibolov@gmail.com>
2021-08-26 17:40:18 +0600

    async/await get contacts

--- getAllContacts.js
+++ getAllContacts.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 const UserContacts = require('./db/models/UserContact');
 
-function getAllContacts() {
+async function getAllContacts() {
   const arr = [];
-  const contacts = UserContacts.find({}).exec();
+  const contacts = await UserContacts.find({}).exec();
   console.log(typeof (contacts));
   console.log(contacts);
   arr.push(contacts.map((x) => x.phone));
